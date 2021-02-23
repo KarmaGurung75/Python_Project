937e8cd5d19a7ea32d24b762a432f7cd888abb41
Karma Gurung <63505288+KarmaGurung75@users.noreply.github.com>
2021-02-23 09:07:07 +0545

    python1.1

---
+++ conditional_statement.py
@@ -0,0 +1,55 @@
+
+#if statement
+'''num=int(input("enter the number:"))
+if num<0:
+    print("the number is negative")
+print('program is over')'''
+
+#wap to find number enter by the user is even? if number is even display the appropate message.
+'''num=int(input("Enter the number:"))
+if num/2:
+    print("the number is even ")
+
+rem=num%2
+if rem==0:
+   print('the number is even')
+print('the program is over')
+'''
+#if-else statement
+'''num=int(input("enter the number"))
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+    '''
+#wap to find the number is positive or negative
+'''num=int(input("Enter the number"))
+if num<0:
+    print('the number is negative')
+else:
+    print("the number is positive")
+'''
+name='pooja'
+if name=='pooja':
+    print("pooja is a boy")
+else:
+    print("pooja is girl")
+
+#if-elif-else statemet
+num=int(input("enter the number:\n"))
+if num==1:
+    print("it is sunday")
+elif num==2:
+    print("it is monday")
+elif num==3:
+    print("it is tuesday")
+elif num==4:
+    print('it is wedneday')
+elif num==5:
+    print("it is thurday")
+elif num==6:
+    print("it is friday")
+elif num==7:
+    print("it is saturday")
+else:
+    print('you think that pooja is a gay')
